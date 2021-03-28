fd262f7abc594cef1816202459134acf1221d488
roottusk <root.tusk@gmail.com>
2021-03-28 22:19:14 +0530

    vAPI 1.0

--- vapi/db/var.php
+++ vapi/db/var.php
@@ -1,9 +1,9 @@
 <?php
 
 $DB_NAME="vapi";
-$DB_HOST="localhost";
-$DB_PASS="";
-$DB_USERNAME="root";
+$DB_HOST="db";
+$DB_PASS="vapi123456";
+$DB_USERNAME="vapi";
 
 $API4_OTP=base64_decode("MTg3Mg==");
 $API4_KEY=base64_decode("Rlp2amFGbE1nVWZucEZKRGhLeC05MnhlWHhfc0NyN1k=");
